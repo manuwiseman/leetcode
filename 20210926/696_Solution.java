2708d9b294cb5e815009bb5ae782a2d764cf3904
Rui Hou <ruihou@Ruis-MacBook-Air.local>
2021-09-26 21:43:07 -0400

    696. Count Binary Substrings 1492. The kth Factor of n
    Signed-off-by: Rui Hou <ruihou@Ruis-MacBook-Air.local>

---
+++ 20210926/696_Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int ans = 0, pre = 0, cur = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+        	if (s.charAt(i) != s.charAt(i - 1)) {
+        		ans += Math.min(pre, cur);
+        		pre = cur;
+        		cur = 1;
+        	} else{
+        		cur++;
+        	}
+        }
+
+        return ans + Math.min(pre, cur);
+    }
+}
