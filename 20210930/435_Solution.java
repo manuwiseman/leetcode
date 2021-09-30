1b5b81978b87dd8df642f57308c0344768ad7aad
Rui Hou <ruihou@Ruis-MacBook-Air.local>
2021-09-30 12:02:49 -0400

    435. Non-overlapping Intervals
    Signed-off-by: Rui Hou <ruihou@Ruis-MacBook-Air.local>

---
+++ 20210930/435_Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        int count = 1, end = intervals[0][1];
+        
+        for(int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
+                continue;
+            }
+            end = intervals[i][1];
+            count++;
+        }
+        
+        return intervals.length - count;
+    }
+}
