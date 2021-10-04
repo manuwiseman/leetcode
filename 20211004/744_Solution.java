7b20aebdabb729bc67614492ef4ebcfcc995c56d
Rui Hou <ruihou@Ruis-MacBook-Air.local>
2021-10-04 13:39:40 -0400

    540. Single Element in a Sorted Array
    Signed-off-by: Rui Hou <ruihou@Ruis-MacBook-Air.local>

--- 20211004/744_Solution.java
+++ 20211004/744_Solution.java
@@ -1,5 +1,14 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        
+        int n = letters.length;
+        int l = 0, r = n - 1;
+
+        while (l <= r) {
+        	int mid = l + (r - l) / 2;
+        	if (letters[mid] <= target) l = mid + 1;
+        	else r = mid - 1;
+        }
+
+        return letters[l % n];
     }
 }
