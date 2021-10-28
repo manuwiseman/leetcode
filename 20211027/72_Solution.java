419acf62d9f64a7d6247bb520d0b34b6fc432967
Rui Hou <ruihou@Ruis-MacBook-Air.local>
2021-10-28 00:55:06 -0400

    72. Edit Distance
    Signed-off-by: Rui Hou <ruihou@Ruis-MacBook-Air.local>

--- 20211027/72_Solution.java
+++ 20211027/72_Solution.java
@@ -4,15 +4,23 @@ class Solution {
         int[][] dp = new int[m + 1][n + 1];
 
         for (int i = 1; i <= m; i++) {
+        	dp[i][0] = i;
+        }
+
+        for (int j = 1; j <= n; j++) {
+        	dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
         	for (int j = 1; j <= n; j++) {
         		if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-        			dp[i][j] = dp[i - 1][j - 1] + 1;
+        			dp[i][j] = dp[i - 1][j - 1];
         		} else {
-        			dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+        			dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
         		}
         	}
         }
 
-        return m + n - 2 * dp[m][n];
+        return dp[m][n];
     }
 }
