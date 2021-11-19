3b42d7c284691ff4f800acf5a7008888b82e6b6d
Rui Hou <manuwiseman@gmail.com>
2021-11-18 23:47:48 -0500

    240. Search a 2D Matrix II 485. Max Consecutive Ones
    Signed-off-by: Rui Hou <manuwiseman@gmail.com>

---
+++ 20211118/240_Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length, x = 0, y = n - 1;
+        while (x < m && y >= 0){
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (matrix[x][y] < target) {
+                x++;
+            } else {
+                y--;
+            }
+        }
+        
+        return false;
+    }
+}
