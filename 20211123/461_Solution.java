1cf8b0ea74ab4ce5579e0cb06287fddb9c45af42
Rui Hou <manuwiseman@gmail.com>
2021-11-23 10:28:45 -0500

    461. Hamming Distance 136. Single Number
    Signed-off-by: Rui Hou <manuwiseman@gmail.com>

---
+++ 20211123/461_Solution.java
@@ -0,0 +1,17 @@
+// class Solution {
+//     public int hammingDistance(int x, int y) {
+//         return Integer.bitCount(x ^ y);
+//     }
+// }
+
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int count = 0;
+        while (xor != 0) {
+            xor &= (xor - 1);
+            count++;
+        }
+        return count;
+    }
+}
