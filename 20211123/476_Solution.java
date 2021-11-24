d4f62d4059c097171f4a5d8f698fb71314044490
Rui Hou <manuwiseman@gmail.com>
2021-11-24 00:52:25 -0500

    476. Number Complement
    Signed-off-by: Rui Hou <manuwiseman@gmail.com>

---
+++ 20211123/476_Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findComplement(int num) {
+        int mask = 1 << 30;
+        while ((num & mask) == 0) mask >>= 1;
+        mask = (mask << 1) - 1;
+        return num ^ mask;
+    }
+}
