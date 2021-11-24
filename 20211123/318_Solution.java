ae1d3e84e341b9e10dc7118856b172ef667e8af7
Rui Hou <manuwiseman@gmail.com>
2021-11-24 02:36:10 -0500

    338. Counting Bits 318. Maximum Product of Word Lengths
    Signed-off-by: Rui Hou <manuwiseman@gmail.com>

---
+++ 20211123/318_Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProduct(String[] words) {
+        int n = words.length;
+        int[] val = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (char c : words[i].toCharArray()) {
+                val[i] |= 1 << (c - 'a');
+            }
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((val[i] & val[j]) == 0) {
+                    ans = Math.max(ans, words[i].length() * words[j].length());
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
