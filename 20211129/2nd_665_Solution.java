fc140f5838aaa0435b18c74a27a6913a407db66c
Rui Hou <manuwiseman@gmail.com>
2021-11-29 16:06:23 -0500

    665. Non-decreasing Array 53. Maximum Subarray
    Signed-off-by: Rui Hou <manuwiseman@gmail.com>

---
+++ 20211129/2nd_665_Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length && count < 2; i++) {
+            if (nums[i] >= nums[i - 1]) continue;
+            count++;
+            if (i - 2 >= 0 && nums[i - 2] > nums[i]) {
+                nums[i] = nums[i - 1];
+            } else {
+                nums[i - 1] = nums[i];
+            }
+        }
+        return count <= 1;
+    }
+}
