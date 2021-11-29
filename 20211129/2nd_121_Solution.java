0ff52dc0f482389143d7002bbc7f1d8e8b1ae5c3
Rui Hou <manuwiseman@gmail.com>
2021-11-29 10:59:47 -0500

    121. Best Time to Buy and Sell Stock 122. Best Time to Buy and Sell Stock II
    Signed-off-by: Rui Hou <manuwiseman@gmail.com>

---
+++ 20211129/2nd_121_Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length < 2) return 0;
+        int ans = 0, min = prices[0];
+        
+        for (int i = 1; i < prices.length; i++) {
+            ans = Math.max(ans, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        
+        return ans;   
+    }
+}
