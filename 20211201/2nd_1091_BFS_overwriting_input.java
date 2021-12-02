c528de0df2a4e3b6d2f194b46c1c65b74f61870c
Rui Hou <manuwiseman@gmail.com>
2021-12-02 01:58:10 -0500

    1091. Shortest Path in Binary Matrix
    Signed-off-by: Rui Hou <manuwiseman@gmail.com>

---
+++ 20211201/2nd_1091_BFS_overwriting_input.java
@@ -0,0 +1,40 @@
+class Solution {
+    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) return -1;
+        Queue<int[]> q = new LinkedList<int[]>();
+        grid[0][0] = 1;
+        q.add(new int[]{0, 0});
+        while (!q.isEmpty()) {
+            int[] element = q.poll();
+            int row = element[0];
+            int col = element[1];
+            int distance = grid[row][col];
+            
+            if (row == n - 1 && col == n - 1) {
+                return distance;
+            }
+            
+            for (int[] neighbor : getNeighbors(grid, row, col)) {
+                int newRow = neighbor[0];
+                int newCol = neighbor[1];
+                q.add(new int[]{newRow, newCol});
+                grid[newRow][newCol] = distance + 1;
+            }
+        }
+        return -1;
+    }
+    
+    private List<int[]> getNeighbors(int[][] grid, int row, int col) {
+        List<int[]> neighbors = new ArrayList<int[]>();
+        for (int[] dir : this.directions) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length && grid[newRow][newCol] == 0) {
+                neighbors.add(new int[]{newRow, newCol});
+            }
+        }
+        return neighbors;
+    }
+}
