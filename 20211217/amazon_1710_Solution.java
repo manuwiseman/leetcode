10f688d31b42066534b96619e848ed0cbe2cbb1f
Rui Hou <manuwiseman@gmail.com>
2021-12-21 07:49:47 -0500

    1710. Maximum Units on a Truck
    Signed-off-by: Rui Hou <manuwiseman@gmail.com>

---
+++ 20211217/amazon_1710_Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        int count = 0;
+        // sort boxTypes array
+        Arrays.sort(boxTypes, (a, b) -> (a[1] - b[1]));
+        
+        for (int i = boxTypes.length - 1; i >= 0 && truckSize >= 0; i--) {
+            count += Math.min(boxTypes[i][0], truckSize) * boxTypes[i][1];
+            truckSize -= boxTypes[i][0];
+        }
+        
+        return count;
+    }
+}
